4287cd041f76d45aedb4e7d688f176d1ac8f79d7
Zahraa Thompson <zahraathompson871@gmail.com>
2025-10-30 15:27:03 +0200

    exercise 2

---
+++ exercise_2_zahraa_thompson.js
@@ -0,0 +1,6 @@
+let number1 = 20
+let number2 = 11
+const number1String = String(number1)
+const sum = number1 + number2
+const product = number1 * number2
+console.log(number1,number2,number1String,sum,product)
