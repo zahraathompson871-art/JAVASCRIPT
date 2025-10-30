703811a4f8480bce3d74274164d8b1ac2cb31566
Zahraa Thompson <zahraathompson871@gmail.com>
2025-10-30 15:07:40 +0200

    exercise1

---
+++ exercise_1_zahraa_thompson.js
@@ -0,0 +1,6 @@
+const firstName = "Zahraa";
+const lastName = "Thompson";
+let age = 19;
+const fullName = `${firstName} ${lastName}`; 
+console.log(`Hello, ${fullName}! Welcome to JavaScript`); 
+age = 20;
