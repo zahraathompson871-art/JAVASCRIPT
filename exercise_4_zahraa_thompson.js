41fc247bad4f850db46b4aa2b01bb4e8d6ecdd0e
Zahraa Thompson <zahraathompson871@gmail.com>
2025-10-31 14:36:10 +0200

    exercise 4

---
+++ exercise_4_zahraa_thompson.js
@@ -0,0 +1,10 @@
+let score=19;
+const passingGrade=60;
+if (score==100) {
+    console.log("Perfect score!");
+}else if(score>=passingGrade){
+    console.log("Passed");
+}else{
+    console.log("Failed");
+}
+
