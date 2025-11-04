5b7c8665bc9f9ceda2085a8299ba18802561eec4
Zahraa Thompson <zahraathompson871@gmail.com>
2025-11-04 15:22:41 +0200

    exercise 6

---
+++ functions.js
@@ -0,0 +1,12 @@
+function greet(name){
+    return "Hello,"+ name + "!";
+}
+
+function add(c,d){
+    return c+d;
+}
+
+let greeting = greet("Zahraa");
+let sum = add(2,5);
+
+console.log(greeting,sum);
