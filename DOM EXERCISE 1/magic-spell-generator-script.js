928db7d94c445cb8d4c2837af7963417ffec66da
Zahraa Thompson <zahraathompson871@gmail.com>
2025-11-24 11:56:17 +0200

    week 5 exercise 1

--- DOM EXERCISE 1/magic-spell-generator-script.js
+++ DOM EXERCISE 1/magic-spell-generator-script.js
@@ -1,12 +1,50 @@
 document.addEventListener('DOMContentLoaded',function(){
 const generateButton = document.getElementById('generateButton');
 const ingredients = document.querySelectorAll('#ingredientsList li');
-const spellarea = document.getElementById('spellArea');
+const spellArea = document.getElementById('spellArea');
+const resestButton = document.getElementById('resetButton')
 
 function generateSpell(){
     const randomIndex = Math.floor(Math.random() *ingredients.length);
     const randomIngredient = ingredients[randomIndex].textContent;
-    spellarea.innerHTML = `<h2>${randomIngredient}</h2>`;
+    spellArea.innerHTML = `<h2>${randomIngredient}</h2>`;
 }
-generateButton.addEventListener('click',generateSpell);
+function getRandomColor(){
+    const r = Math.floor(Math.random()*256);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
+    return `rgb(${r}, ${g},${b})`;
+}
+
+function changeSpellColor(){
+    spellArea.style.backgroundColor = getRandomColor();
+}
+
+function countdown(){
+    let countdown = 3;
+    generateButton.disabled = true;
+    spellArea.innerHTML = `<h2>${countdown}</h2>`;
+
+    const countdownInterval =setInterval(()=>{
+        countdown--;
+        spellArea.innerHTML = `<h2>${countdown}</h2>`;
+
+        if(countdown === 0){
+            clearInterval(countdownInterval);
+            generateSpell();
+            changeSpellColor();
+            generateButton.disabled = false;
+        }
+    },1000);
+}
+
+function reset(){
+    spellArea.innerHTML = '<h2>Spell Area</h2>';
+    spellArea.style.backgroundColor = '';
+}
+
+reset();
+generateButton.addEventListener('click', countdown);
+resestButton.addEventListener('click',reset)
+  
 });
