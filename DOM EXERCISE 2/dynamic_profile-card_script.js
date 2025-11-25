bed02064e36cdd1fdfdace0552df8f12f2c48546
Zahraa Thompson <zahraathompson871@gmail.com>
2025-11-25 13:03:03 +0200

    week 5 exercise 2

--- DOM EXERCISE 2/dynamic_profile-card_script.js
+++ DOM EXERCISE 2/dynamic_profile-card_script.js
@@ -1,3 +1,38 @@
+document.addEventListener('DOMContentLoaded',function(){
 const updateNameBtn=document.getElementById('updateNameBtn')
+const profileName=document.getElementById('profileName')
+const updateRoleBtn=document.getElementById('updateRoleBtn')
+const profileRole=document.getElementById('profileRole')
+const toggleStatusBtn=document.getElementById('toggleStatusBtn')
+const profileCard=document.getElementById('profileCard')
+const changeImageBtn=document.getElementById('changeImageBtn')
+const profileImage=document.getElementById('profileImage')
 
-function ()
+
+function updateName(){
+let name = prompt("Enter your name");
+profileName.innerHTML = `<h2>${name}</h2>`;
+};
+updateNameBtn.addEventListener('click', updateName);
+
+
+function updateRole(){
+let role = prompt("Enter your role");
+profileRole.innerHTML = `<p>${role}</p>`;
+};
+updateRoleBtn.addEventListener('click', updateRole);
+
+
+function toggleStatus(){
+    profileCard.classList.toggle('active-status')
+};
+toggleStatusBtn.addEventListener('click',toggleStatus);
+
+function changeImg(){
+    let img = prompt("Paste url for image");
+    profileImage.src = img;
+};
+
+changeImageBtn.addEventListener('click',changeImg);
+
+});
