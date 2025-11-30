2a2d76aadbcb480e8b3fb0febc58ff6e8fda0210
Zahraa Thompson <zahraathompson871@gmail.com>
2025-11-30 13:46:53 +0200

    week 5 exercise 3

--- DOM EXERCISE 3/js/main.js
+++ DOM EXERCISE 3/js/main.js
@@ -4,37 +4,37 @@ const password = document.getElementById('pwd');
 const errorModal = document.querySelector('.modal');
 const toggleButton = document.querySelector('.btn-toggle-nav')
 const sidebar = document.querySelector('.nav-sidebar')
+const sidebarUL =sidebar.querySelector('ul')
 
 errorModal.style.display = 'none';
 
 function validateLogin(event){
     event.preventDefault();
-    console.log("validateLogin function is running")
 
     if(username.value === 'admin' && password.value === 'password123'){
         window.location.href = 'index.html';
     }else{
-        console.log("login failed -should show modal")
         showModal();
     }
 }
 
 function showModal(){
-    console.log("showmodal is running")
     errorModal.style.display = 'block';
 }
 
-function hideModal(){
+function dismissModal(){
     errorModal.style.display = 'none';
 }
 
-errorModal.addEventListener('click',function(event){
-    if(event.target === errorModal){
-        hideModal();
+function toggleNav(){
+    if(sidebarUL.style.visibility === 'visible'){
+        sidebarUL.style.visibility = 'hidden';
+        sidebar.style.width = '50px';
+    }else{
+        sidebarUL.style.visibility = 'visible';
+        sidebar.style.width = '272px';
     }
-});
 
-toggleButton.addEventListener('click',function(){
-    sidebar.classList.toggle('sidebar-open');
-    toggleButton.classList.toggle('rotated');
-});
+    toggleButton.style.transform = toggleButton.style.transform === 'rotate(90deg)' ? 'rotate(0deg)' : 'rotate(90deg)';
+    
+}
